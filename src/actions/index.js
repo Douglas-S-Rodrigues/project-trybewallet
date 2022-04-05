568f9af25a6c32fd484ae269972274561199521a
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-04-05 14:28:27 -0300

    test

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,7 @@ export const currencies = (payload) => ({ type: CURRENCIE, payload });
 export const getCurrencie = () => async (dispatch) => {
   const promise = await fetch('https://economia.awesomeapi.com.br/json/all');
   const data = await promise.json();
+
   const currencieValue = Object.keys(data).filter((info) => !info.includes('USDT'));
 
   dispatch(currencies(currencieValue));
